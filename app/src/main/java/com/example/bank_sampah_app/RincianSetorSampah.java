df0bfb1fd314d1a668cc4a0b067a033001cc3d6e
ASUS <kevinprmn25@gmail.com>
2022-11-28 01:44:01 +0800

    setor sampah 2

---
+++ app/src/main/java/com/example/bank_sampah_app/RincianSetorSampah.java
@@ -0,0 +1,14 @@
+package com.example.bank_sampah_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RincianSetorSampah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rincian_setor_sampah);
+    }
+}
