68c780be2b3e5a35f57fce0e609dc9fbab7930a2
hridayanikaruna <hridayani.karuna@gmail.com>
2022-12-01 16:47:01 +0800

    api starter login register

---
+++ app/src/main/java/com/example/bank_sampah_app/API/UserService.java
@@ -0,0 +1,24 @@
+package com.example.bank_sampah_app.API;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface UserService {
+
+
+//    @POST("login")
+//    Call<LoginResponse> userLogin(@Body LoginRequest loginRequest);
+//
+//
+//    @POST("register")
+//    Call<RegisterResponse> userRegister(@Body RegisterRequest registerRequest);
+//
+//
+//    @POST("transaksi/create")
+//    Call<OrderResponse> userOrder(@Body OrderRequest orderRequest);
+}
