a6e5f1c6bc98fce0d466eb04a34c99565fe3e77d
hridayanikaruna <hridayani.karuna@gmail.com>
2022-12-02 19:46:46 +0800

    upgrade setor sampah

--- app/src/main/java/com/example/bank_sampah_app/SetorSampah.java
+++ app/src/main/java/com/example/bank_sampah_app/SetorSampah.java
@@ -7,7 +7,40 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 
-public class SetorSampah extends AppCompatActivity {
+public class SetorSampah extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner jenisPenyotaranSpinner;
+    String[] jenisPenyetoranSampahString;
+    ArrayAdapter<String> jenisPenyetoranAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setor_sampah);
+
+        //spinner pilihan jenis penyetoran
+        jenisPenyotaranSpinner = findViewById(R.id.jenisPenyetoranSpinner);
+
+        //adapter spinner
+        jenisPenyetoranAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,jenisPenyetoranSampahString);
+        jenisPenyotaranSpinner.setAdapter(jenisPenyetoranAdapter);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
+        Spinner spinner = (Spinner) parent;
+        if(spinner.getId() == R.id.jenisPenyetoranSpinner)
+        {
+            //
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 
 }
