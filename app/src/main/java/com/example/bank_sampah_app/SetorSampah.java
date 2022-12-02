bf62f445c9471d121bec8e42d0803327639bb0d5
ASUS <kevinprmn25@gmail.com>
2022-12-02 18:45:51 +0800

    otw push

---
+++ app/src/main/java/com/example/bank_sampah_app/SetorSampah.java
@@ -0,0 +1,13 @@
+package com.example.bank_sampah_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class SetorSampah extends AppCompatActivity {
+
+}
