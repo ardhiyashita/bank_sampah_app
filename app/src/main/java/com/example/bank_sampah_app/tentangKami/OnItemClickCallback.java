8189dc447ab07659d8021912bfe32a0ccb523f31
ddwikasari <79885421+ddwikasari@users.noreply.github.com>
2023-01-08 14:32:23 +0800

    Delete app directory

--- app/src/main/java/com/example/bank_sampah_app/tentangKami/OnItemClickCallback.java
+++
@@ -1,7 +0,0 @@
-package com.example.bank_sampah_app.tentangKami;
-
-import com.example.bank_sampah_app.help.HelpItem;
-
-public interface OnItemClickCallback {
-    void onItemClicked(HelpItem helpItem);
-}
